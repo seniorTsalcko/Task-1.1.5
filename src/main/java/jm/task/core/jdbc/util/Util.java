f8ba125a0c8d893e9a7dc55e22e001067fcfa829
seniorTsalcko <seniorTsalcko@yandex.ru>
2024-01-15 12:02:02 +0300

    Tabulation Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,38 +7,36 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
-    public class Util {
-        private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-        private static final String HOST = "jdbc:mysql://localhost:3306/mydb";
-        private static final String LOGIN = "username";
-        private static final String PASSWORD = "root";
-        private static SessionFactory sessionFactory = null;
+public class Util {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/mydb";
+    private static final String LOGIN = "username";
+    private static final String PASSWORD = "root";
+    private static SessionFactory sessionFactory = null;
 
-        public static SessionFactory getConnection() {
+    public static SessionFactory getConnection() {
 
-            try {
-                Configuration configuration = new Configuration()
-                        .setProperty("hibernate.connection.driver_class", DRIVER)
-                        .setProperty("hibernate.connection.url", HOST)
-                        .setProperty("hibernate.connection.username", LOGIN)
-                        .setProperty("hibernate.connection.password", PASSWORD)
-                        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-                        .addAnnotatedClass(User.class)
-                        .setProperty("hibernate.c3p0.min_size", "5")
-                        .setProperty("hibernate.c3p0.max_size", "200")
-                        .setProperty("hibernate.c3p0.max_statements", "200");
+        try {
+            Configuration configuration = new Configuration().
+                    setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST).
+                    setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size", "5")
+                    .setProperty("hibernate.c3p0.max_size", "200")
+                    .setProperty("hibernate.c3p0.max_statements", "200");
 
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (HibernateException e) {
-                e.printStackTrace();
-            }
-            return sessionFactory;
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
         }
+        return sessionFactory;
     }
+}
